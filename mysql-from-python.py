d51cf7dea896acee2d92fe5bf534f05e0382a58f
AledPeart <aledpeart@gmail.com>
2021-09-18 11:56:29 +0000

    Cursor lesson updates

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -8,12 +8,13 @@ username = os.getenv('C9_USER')
 connection = pymysql.connect(host='localhost', user=username, password='', db='Chinook')
 
 try:
-    #ren a query
+    #run a query
     with connection.cursor() as cursor:
         sql = "SELECT * FROM Artist;"
         cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        for row in cursor: 
+            print(row)
 finally:
     # Close the connection, regardless of whether the above was succesful
     connection.close()
+
